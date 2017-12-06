0d6036cbad48d83dac829c337515674abbcee082
anLA7856 <350493391@qq.com>
2017-12-07 07:47:30 +0800

    first upload in 12/7/2017

---
+++ src/main/java/anLA7856/activiti/App.java
@@ -0,0 +1,13 @@
+package anLA7856.activiti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
